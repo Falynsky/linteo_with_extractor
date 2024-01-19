b18d3995d72b067177f904e96ee663564800b453
Kamil Faliński <kamil.falinski@iteo.com>
2024-01-19 10:28:59 +0100

    test

--- linteo_extractor/lib/on_review_approved/on_review_approved_runner.dart
+++ linteo_extractor/lib/on_review_approved/on_review_approved_runner.dart
@@ -17,7 +17,7 @@ class OnReviewApprovedRunner extends ActionRunner {
   @override
   void runAction() {
     final helper = VersionHelper(workspace);
-    final version = helper.getIncrementedVersion();
+    final String  version = helper.getIncrementedVersion();
 
     _tryUpdateVersionedFile(
       workspace: workspace,
