3a81e02593828f4bd378d09719ad51e64644022c
Kamil Faliński <kamil.falinski@iteo.com>
2024-01-22 13:48:41 +0100

    test

--- linteo_extractor/lib/on_review_approved/rules_parser.dart
+++ linteo_extractor/lib/on_review_approved/rules_parser.dart
@@ -1,4 +1,7 @@
 const _unselectedTick = '- [ ]';
+const _selectedTick = '- [x]';
+final _deletedLinkMark = RegExp(r'\d+\.');
+final _deletedLinkMarkWithSpacebar = RegExp(r'\d+\.\s');
 
 class RulesParser {
   static List<String> getUnselectedRules(String input) => input
@@ -6,6 +9,20 @@ class RulesParser {
       .where(containsUnselectedRule)
       .map((rule) => rule.replaceFirst('$_unselectedTick ', '').trim())
       .toList();
+
+  static List<String> getSelectedRules(String input) => input
+      .split('\n')
+      .where(containsUnselectedRule)
+      .map((rule) => rule.replaceFirst('$_selectedTick ', '').trim())
+      .toList();
+
+  static List<String> getDeletedRules(String input) => input
+      .split('\n')
+      .where(containsDeletedRule)
+      .map((rule) => rule.replaceFirst(_deletedLinkMarkWithSpacebar, '').trim())
+      .toList();
+
+  static bool containsDeletedRule(String input) => _deletedLinkMark.hasMatch(input);
 
   static bool containsUnselectedRule(String input) => input.contains(_unselectedTick);
 }
