64a724e6ae5e29918e5590886e608f9a95b08db5
Kamil Faliński <kamil.falinski@iteo.com>
2024-01-22 13:53:52 +0100

    test

--- linteo_extractor/lib/on_review_approved/rules_parser.dart
+++ linteo_extractor/lib/on_review_approved/rules_parser.dart
@@ -1,4 +1,9 @@
+import 'package:collection/collection.dart';
+
 const _unselectedTick = '- [ ]';
+const _selectedTick = '- [x]';
+final _deletedLinkMark = RegExp(r'\d+\.');
+final _deletedLinkMarkWithSpacebar = RegExp(r'\d+\.\s');
 
 class RulesParser {
   static List<String> getUnselectedRules(String input) => input
@@ -7,5 +12,20 @@ class RulesParser {
       .map((rule) => rule.replaceFirst('$_unselectedTick ', '').trim())
       .toList();
 
+  static List<String> getSelectedRules(String input) => input
+      .split('\n')
+      .where(containsSelectedRule)
+      .map((rule) => rule.replaceFirst('$_selectedTick ', '').trim())
+      .toList();
+
+  static List<String> getDeletedRules(String input) => input
+      .split('\n')
+      .where(containsDeletedRule)
+      .map((rule) => rule.replaceFirst(_deletedLinkMarkWithSpacebar, '').trim())
+      .toList();
+
+  static bool containsDeletedRule(String input) => _deletedLinkMark.hasMatch(input);
+
   static bool containsUnselectedRule(String input) => input.contains(_unselectedTick);
+  static bool containsSelectedRule(String input) => input.contains(_selectedTick);
 }
