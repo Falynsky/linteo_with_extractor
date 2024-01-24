9c1c5c28ee7a2e77f23b63f9c7dbf624a637f2a0
Kamil Faliński <kamil.falinski@iteo.com>
2024-01-24 13:18:15 +0100

    TEST

--- linteo_extractor/test/extractor_test.dart
+++ linteo_extractor/test/extractor_test.dart
@@ -55,7 +55,7 @@ void main() {
       final htmlHash = htmlExtractor.getLintsHash();
       final yamlHash = yamlExtractor.getLintsHash();
 
-      expect(htmlHash, 432);
+      expect(htmlHash, yamlHash);
     });
   });
 }
